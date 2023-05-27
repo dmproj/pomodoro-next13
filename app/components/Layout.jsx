d8a4bd707d9107a026a7a41a16cdc369bf68e6c2
SY <seyav29@gmail.com>
2023-05-27 10:39:24 -0400

    first commit

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,3 +1,4 @@
+"use client"
 
 const Layout = () => {
   return (
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,24 @@
+"use client";
+
+import styles from "../styles/Navbar.module.css";
+import { LocalPhone, LocalPhoneOutlined } from "@mui/icons-material";
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <LocalPhoneOutlined />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>+1 234 56789</div>
+        </div>
+      </div>
+      <div className={styles.item}>center</div>
+      <div className={styles.item}>right</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
